51f72266069fe107d22ce6779bdaabcee8b3d80d
AntipovSergey <72650361+AntipovSergey@users.noreply.github.com>
2021-03-16 18:52:06 +0300

    Доделал сайт (добавил минифицированные css и js)

--- js/catalogScript.js
+++ js/catalogScript.js
@@ -1,16 +1,20 @@
 //Modal catalog hide
 
-let purchaseButton = document.querySelector('.purchase-button');
+let purchaseButtons = document.querySelectorAll('.purchase-button');
 let modalCatalog = document.querySelector('.modal-catalog-hide');
-let catalogHide = document.querySelector('.button-close-catalog')
+let catalogHide = document.querySelector('.button-close-catalog');
 
-purchaseButton.addEventListener('click', function(evt) {
+const openModal = (evt) => {
   evt.preventDefault();
   modalCatalog.classList.remove('modal-catalog-hide');
   modalCatalog.classList.add('modal-catalog-show');
-});
+}
 
-catalogHide.addEventListener('click', function(evt) {
+for (let item of purchaseButtons) {
+  item.addEventListener('click', openModal);
+}
+
+catalogHide.addEventListener('click', (evt) => {
   evt.preventDefault();
   modalCatalog.classList.remove('modal-form-show');
   modalCatalog.classList.add('modal-catalog-hide');
